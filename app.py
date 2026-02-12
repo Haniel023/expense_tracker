b8eaf9d1d33cad70a4e198f5cf16b4737f05c95e
Jether Haniel De Leon <54757754+Haniel023@users.noreply.github.com>
2026-02-12 11:04:13 +0800

    adding app.py for backend

--- app.py
+++ app.py
@@ -33,4 +33,54 @@ def index():
                     """).fetchall()
     conn.close()
 
-    total_income = sum(r["amount"] for r in rows if r ["type"] == "income")
+    total_income = sum(r["amount"] for r in rows if r["type"] == "income")
+    total_expense = sum(r["amount"] for r in rows if r["type"] == "expense")
+    balance = total_income - total_expense
+
+    return render_template(
+        "index.html",
+        transaction=rows,
+        total_income=total_income,
+        total_expense=total_expense,
+        balance=balance
+    )
+
+@app.route("/add", methods=["POST"])
+def add_transaction():
+    t_type = request.form.get("type")
+    desc = request.form.get("description", "").strip()
+    amount_str = request.form.get("amount", "").strip()
+
+    #basic validation
+    if t_type not in ("income", "expense"):
+        return redirect(url_for("index"))
+    if not desc:
+        return redirect(url_for("index"))
+    try:
+        amount = float(amount_str)
+        if amount < 0:
+            return redirect(url_for("index"))
+    except ValueError:
+        return redirect(url_for("index"))
+    
+    conn = get_db_connection()
+    conn.execute("""
+                 INSERT INTO transactions (type, description, amount)
+                 VALUES (?, ?, ?)
+            """, (t_type, desc, amount))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:txn_id>", methods=["POST"])
+def delete_transaction(txn_id):
+    conn = get_db_connection()
+    conn.execute("DELETE FROM transactions WHERE id = ?", (txn_id))
+    conn.commit()
+    conn.close()
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
